8a54ffb42e784cbe23c30cac66ea88310e7d2d3d
Dimas Camillo <dimas.camillo@sonda.com>
2022-12-11 14:20:08 -0300

    Finalizada a Aula 01

---
+++ public/assets/controller/validacaoForm.js
@@ -0,0 +1,32 @@
+const formInputNome = document.querySelector(".form__input--nome");
+const formInputEmail = document.querySelector(".form__input--email");
+const form = document.querySelector("form");
+const btnEnviar = document.querySelector(".form__btn");
+let msg = document.createElement("span");
+
+function validaForm() {
+
+  btnEnviar.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (formInputNome.value == "" && formInputEmail.value == "") {
+      mensagemError();
+    } else if (!formInputNome.value == "" && !formInputEmail.value == "") {
+      mensagemSucesso();
+    }
+  });
+}
+
+function mensagemError() {
+  msg.innerHTML = "Por favor preencha todos os campos!";
+  form.appendChild(msg);
+  btnEnviar.classList.add('disable')
+}
+
+function mensagemSucesso() {
+  msg.innerHTML = "Formulario enviado com sucesso!";
+  form.appendChild(msg);
+  btnEnviar.classList.remove('disable')
+}
+
+validaForm();
